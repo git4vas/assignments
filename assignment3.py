3cc4fb5edcd236b01706e5e480324d6c4b7ad4d3
Vasily Neganov <mail@mail.fr>
2022-04-20 02:01:40 +0200

    assignment3 as I liked it--with 'an' and float

--- assignment3.py
+++ assignment3.py
@@ -2,21 +2,29 @@ x = float(input("Please enter the first angle:"))
 y = float(input("Please enter the second angle:"))
 z = float(input("Please enter the third angle:"))
 
-if (x > 0 and y > 0 and z > 0 and x+y+z==180):
-    if (x >= y and x >= z):
-        largest = x
-    elif y >= z:
-        largest = y
+if (x > 0 and y > 0 and z > 0):
+    if (x+y+z==180):
+        if (x >= y and x >= z):
+            largest = x
+        elif y >= z:
+            largest = y
+        else:
+            largest = z
+
+            if largest > 90:
+                triangle = "n obtuse"
+            elif largest == 90:
+                triangle = " right"
+            else:
+                triangle = "n acute" 
+
+            print("The triangle is a" + triangle, "triangle")    
+
     else:
-        largest = z
-else:
-    print("not an euclidean triangle")
+        print("The entered values are not valid.")
 
-if largest > 90:
-    type = "obtuse"
-elif largest == 90:
-    type = "right"
 else:
-    type = "acute" 
+    print("Angles smaller than 0 are not valid.")
 
-print("The triangle is", type)    
+
+
