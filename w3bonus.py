adc7966f1e0ab82ec2b46ae133ed1a3a292c6219
Vasily Neganov <mail@mail.fr>
2022-05-04 04:05:43 +0200

    w3bonus

---
+++ w3bonus.py
@@ -0,0 +1,22 @@
+shift = input("shift (0-25): ")
+while not(shift.isdecimal()) or int(shift) > 25 or int(shift) < 0:
+    print(shift, "is not a number between 0 and 25")
+    shift = (input("shift (0-25): "))
+
+plaintext = str.lower(input("Enter text to encrypt > "))
+#plaintext = "programming: Python is fun!"
+
+i = 0
+encrypted = ""
+
+for ltr in plaintext:
+    if ltr.isalpha():
+        if ord(ltr) + int(shift) <= ord("z"):
+            encrypted += chr(ord(ltr) + int(shift))
+        else:
+            encrypted += chr(ord(ltr) + int(shift) - 26)
+    else:
+        encrypted += ltr
+    i += 1
+
+print(encrypted)
