20d4179670bc3ed44673fdfeb1d9da41bb572f34
Vasily Neganov <mail@mail.fr>
2022-05-04 02:26:41 +0200

    two ways to solve w3 assignment

--- w3assignment.py
+++ w3assignment.py
@@ -1,27 +1,31 @@
 plaintext = str.lower(input("Enter text to encrypt > "))
-#plaintext = "programming python is fun!"
-
+#plaintext = "programming: Python is fun!"
+encrypted = ""
 #shift = input("shift: ")
 shift = 5
-
-dictionary = {" ": " ", ",": ",", ".": ".", "!": "!", "?": "?", "-": "-", ";": ";", ":": ":"}
-encrypted = ""
-letters = "abcdefghijklmnopqrstuvwxyz"
 i = 0
-j = 0
-k = 0
-l = 0
-
-#print(len(letters))
-
-for ltr in letters:
-    if i + shift + 1 <= len(letters):
-        dictionary[ltr] = letters[(i + shift)]
-    else:
-        dictionary[ltr] = letters[(i - len(letters) + shift)]
-    i += 1
-#print(dictionary)
 
 for ltr in plaintext:
-    encrypted = encrypted + dictionary[ltr]
+    if ltr.isalpha():
+        if ord(ltr) + shift <= ord("z"):
+            encrypted += chr(ord(ltr) + shift)
+        else:
+            encrypted += chr(ord(ltr) + shift - 26)
+    else:
+        encrypted += ltr
+    i += 1
+
+#dictionary = {" ": " ", ",": ",", ".": ".", "!": "!", "?": "?", "-": "-", ";": ";", ":": ":"}
+#letters = "abcdefghijklmnopqrstuvwxyz"
+#print(len(letters))
+#for ltr in letters:
+#    if i + shift + 1 <= len(letters):
+#        dictionary[ltr] = letters[(i + shift)]
+#    else:
+#        dictionary[ltr] = letters[(i - len(letters) + shift)]
+#    i += 1
+#print(dictionary)
+#for ltr in plaintext:
+#    encrypted = encrypted + dictionary[ltr]
+
 print(encrypted)
